c6595b132e6936c24b2517857577d38f62f91566
chaoli <chaoli@wustl.edu>
2016-06-02 13:55:08 +0800

    v1.1

---
+++ 1.1-is unique.java
@@ -0,0 +1,30 @@
+//The String is ASCII or Unicode? 
+
+//algorithm 1
+boolean isUniqueChars(string str){
+    if(str.length() >128)
+        return false;
+    boolean[] char_set = new boolean[128];
+    for (i = 0; i < str.length(); i++){
+        int val = str.charAt(i);
+        if (char_set[val])   // this char has already existed in String.
+            return false;
+        char_set[val]=true;
+    }
+    return true;
+
+}
+
+
+//algorithm 2
+boolean isUniqueChars(string str){
+    int checker = 0;
+    for (int i = 0; i < str.length(); i++){
+        int val = str.charAt(i) - 'a';
+        if ((checker & (1 << val)) > 0){
+            return false
+        }
+        checker |= (1<<val);
+    }
+    return true;
+}
