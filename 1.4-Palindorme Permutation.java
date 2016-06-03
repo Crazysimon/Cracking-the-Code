a904d29787bfa14a8173875037d9794263bafc2d
chaoli <chaoli@wustl.edu>
2016-06-03 15:14:19 +0800

    v1.0

---
+++ 1.4-Palindorme Permutation.java
@@ -0,0 +1,45 @@
+//Algorithm 1: A String can have no more than one character that is odd.
+/*
+use a hash table to count how many times each character appears then iterate it though make sure that no more than one character has an odd count.
+*/
+
+boolean isPermutationOfPalindrome(String phrase){
+    int[] hashtable = buildCharFrequencyTable(phrase);
+    return checkMaxOneOdd(hashtable);
+    }
+    
+//count how many times each charactor appear.
+
+int[] buildCharFrequencyTable(String phrase){
+    int[] hashtable = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1 ];
+    for (char c : phrase.toCharArray){
+        int x =getCharNumber(c);
+        if (x != -1){
+            hashtable[x]++;
+        }
+    }
+    return hashtable;
+}
+
+int getCharNumber(Character c){
+    int a = Character.getNumericValue('a');
+    int z = Character.getNumericValue('z');
+    int val = Character.getNumericValue(c);
+    if (a <=val && val <= z){
+        return val - a;
+    }
+    return -1;
+}
+
+boolean checkMaxOneOdd(int[] table){
+    boolean foundOdd = false;
+    for (int count : table){
+        if (count %2 == 1){
+            if(foundOdd){
+                return false;
+            }
+        foundOdd = true;
+        }
+    }
+    return true;
+}
