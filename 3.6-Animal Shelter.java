7e8973ce3a6956b70ccd7d33cb6c3f96e399037f
chaoli <chaoli@wustl.edu>
2016-07-01 16:29:47 +0800

    v1.0

---
+++ 3.6-Animal Shelter.java
@@ -0,0 +1,67 @@
+//use separate queues for fogs and cats, then store some sort of of timestamp to mark when each animal was queued.
+
+abstract class Animal{
+    private int order;
+    protected String name;
+    public Animal(String n){ name = n;}
+    public cvoid setOrder(int ord){ order = ord; }
+    public int getOrder(){return order;}
+    
+    /*compare orders of animals to return the older item.*/
+    public boolean isOlderThan(Animal a){
+        return this.order < a.getOrder();
+    }
+    
+}
+
+class AnimalQueue{
+    LinkedList<Dog> dogs = new LinkedList<Dog>();
+    LinkedList<Cat> cats = new LinkedList<Cat>();
+    private int order = 0 ;// act as timestamp
+    
+    public void enqueue(Animal a){
+        /*Order is used as a sort of timestamp, so that we can compare the insertion order of a dog or cat.*/
+        a.setOrder(order);
+        order ++;
+        
+        if(a instanceof Dog) dogs.addLast((Dog)a);
+        else if(a instanceof Cat) cats.addLast((Cat)a);
+    }
+    
+    public Animal dequeueAny(){
+        /*Look at tops of dog and cat queues, and pop the queue with the oldest value*/
+        if(dogs.size() == 0){
+            return dequeueCats();
+        }
+        else if(cats.size() == 0){
+            return dequeueDogs();
+        }
+        
+        Dog dog = dogs.peek();
+        Cat cat = cats.peek();
+        if(dog.isOlderThan(cat)){
+            return dequeueDogs();
+        }
+        else{
+            return dequeueCats();
+        }
+    }
+    
+    public Dog dequeueDogs(){
+        return dogs.poll();
+    }
+    
+    public Cat dequeueCats(){
+        return cats.poll();
+    }
+}
+public class Dog extends Animal{
+    public Dog(String n){super(n);}
+        
+}
+
+public class Cat extends Animal{
+    public Cat(String n){super(n);}
+}
+    
+
